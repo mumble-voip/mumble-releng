ecd59290c55af45109613cfe39004eef00e51250
Mikkel Krautz <mikkel@krautz.dk>
2013-08-02 11:48:43 +0200

    buildenv/osx: explicitly build libxar without LZMA support.
    Some people have LZMA installed via Homebrew/MacPorts/themselves.
    We don't want any outside deps, so disable LZMA explicitly.

--- buildenv/osx/build-libxar.bash
+++ buildenv/osx/build-libxar.bash
@@ -4,6 +4,6 @@ git clone https://github.com/mkrautz/xar xar.git
 cd xar.git/xar
 export CFLAGS="-I$OSX_SDK/usr/include/libxml2 ${CFLAGS} -I${MUMBLE_PREFIX}/include/"
 export LDFLAGS="${LDFLAGS} -L${MUMBLE_PREFIX}/lib/"
-./autogen.sh --prefix=${MUMBLE_PREFIX} --disable-shared --enable-static
+./autogen.sh --prefix=${MUMBLE_PREFIX} --disable-shared --enable-static --without-lzma
 make
 make install
