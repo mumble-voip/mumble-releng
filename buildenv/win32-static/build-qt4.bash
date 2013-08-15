18dbb363f64535f388d83282cdfeb06753f90e8e
Mikkel Krautz <mikkel@krautz.dk>
2013-08-15 23:53:50 +0200

    buildenv/win32-static: add MySQL support to Qt 4 via mariadbclient.

--- buildenv/win32-static/build-qt4.bash
+++ buildenv/win32-static/build-qt4.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
 if [ -d mumble-developers-qt ]; then
 	cd mumble-developers-qt
+	git reset --hard
+	git clean -dfx
 else
 	git clone git://gitorious.org/+mumble-developers/qt/mumble-developers-qt.git
 	cd mumble-developers-qt
 	git branch -t 4.8-mumble origin/4.8-mumble
 	git checkout 4.8-mumble
-fi	
+fi
 
-cmd /c configure -release -static -prefix $(cygpath -w ${MUMBLE_PREFIX}/Qt4.8) -qt-sql-sqlite -no-sql-mysql -no-qt3support -no-exceptions -qt-zlib -qt-libpng -qt-libjpeg -openssl-linked -I $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/include) -L $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/lib) OPENSSL_LIBS="-llibeay32 -lssleay32 -lcrypt32" -platform win32-msvc2010 -no-dbus -nomake demos -nomake examples -no-webkit -ltcg -mp -opensource -confirm-license
+patch -p1 < ../patches/qt4-mariadb-support.patch
+
+cmd /c configure -release -static -prefix $(cygpath -w ${MUMBLE_PREFIX}/Qt4.8) -qt-sql-sqlite -qt-sql-mysql -I $(cygpath -w ${MUMBLE_PREFIX}/mariadbclient/mariadbclient/include) -L $(cygpath -w ${MUMBLE_PREFIX}/mariadbclient/lib) -no-qt3support -no-exceptions -qt-zlib -qt-libpng -qt-libjpeg -openssl-linked -I $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/include) -L $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/lib) OPENSSL_LIBS="-llibeay32 -lssleay32 -lcrypt32" -platform win32-msvc2010 -no-dbus -nomake demos -nomake examples -no-webkit -ltcg -mp -opensource -confirm-license
 cmd /c nmake
 cmd /c nmake install
