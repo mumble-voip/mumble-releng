b649c2f294aa60e75d6c6c8993857f8520f99237
Mikkel Krautz <mikkel@krautz.dk>
2013-08-16 21:53:57 +0200

    buildenv/win32-static: generate .pdb for libexpat.

--- buildenv/win32-static/build-expat.bash
+++ buildenv/win32-static/build-expat.bash
@@ -9,7 +9,7 @@ fi
 tar -zxf expat-2.1.0.tar.gz
 cd expat-2.1.0
 cd lib
-export CFLAGS="/nologo /DCOMPILED_FROM_DSP /DXML_BUILDING_EXPAT /c"
+export CFLAGS="/nologo /DCOMPILED_FROM_DSP /DXML_BUILDING_EXPAT /Zi /Fdlibexpat.pdb /c"
 cmd /c cl.exe ${CFLAGS} xmlparse.c xmlrole.c xmltok.c
 cmd /c lib.exe xmlparse.obj xmlrole.obj xmltok.obj /out:libexpat.lib
 mkdir -p ${MUMBLE_PREFIX}/expat/{lib,include}
