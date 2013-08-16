99ba88f847c28ac3e7c06a43d5824383f8c30fdb
Mikkel Krautz <mikkel@krautz.dk>
2013-08-16 23:57:40 +0200

    buildenv/win32-static: generate PDBs for ZeroC Ice.

--- buildenv/win32-static/build-zeroc-ice.bash
+++ buildenv/win32-static/build-zeroc-ice.bash
@@ -16,5 +16,10 @@ export ICE_ARFLAGS="/ignore:4221"
 export ICE_LDFLAGS="user32.lib gdi32.lib dbghelp.lib wsock32.lib ws2_32.lib iphlpapi.lib mcpp.lib libbz2.lib libexpat.lib libeay32.lib libdb53.lib /LIBPATH:$(cygpath -w ${MUMBLE_PREFIX}/mcpp) /LIBPATH:$(cygpath -w ${MUMBLE_PREFIX}/bzip2/lib) /LIBPATH:$(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/lib) /LIBPATH:$(cygpath -w ${MUMBLE_PREFIX}/expat/lib) /LIBPATH:$(cygpath -w ${MUMBLE_PREFIX}/berkeleydb/lib)"
 export ICE_CPPFLAGS="/DXML_STATIC /I$(cygpath -w ${MUMBLE_PREFIX}/bzip2/include) /I$(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/include) /I$(cygpath -w ${MUMBLE_PREFIX}/expat/include) /I$(cygpath -w ${MUMBLE_PREFIX}/berkeleydb/include)"
 
-cmd /c nmake /f Makefile.mak PREFIX="$(cygpath -w ${MUMBLE_PREFIX}/ZeroC-Ice)" STATICLIBS=yes OPTIMIZE=yes GENERATE_PDB=no HAS_MFC=no ARFLAGS="${ICE_ARFLAGS}" LDFLAGS="${ICE_LDFLAGS}" CPPFLAGS="${ICE_CPPFLAGS}"
-cmd /c nmake /f Makefile.mak PREFIX="$(cygpath -w ${MUMBLE_PREFIX}/ZeroC-Ice)" STATICLIBS=yes OPTIMIZE=yes GENERATE_PDB=no HAS_MFC=no ARFLAGS="${ICE_ARFLAGS}" LDFLAGS="${ICE_LDFLAGS}" CPPFLAGS="${ICE_CPPFLAGS}" install
+# Build with RELEASEPDBS=yes to have cl.exe generate .pdbs
+cmd /c nmake /f Makefile.mak PREFIX="$(cygpath -w ${MUMBLE_PREFIX}/ZeroC-Ice)" STATICLIBS=yes OPTIMIZE=yes RELEASEPDBS=yes HAS_MFC=no ARFLAGS="${ICE_ARFLAGS}" LDFLAGS="${ICE_LDFLAGS}" CPPFLAGS="${ICE_CPPFLAGS}"
+# Perform install with RELEASEPDBS=no to avoid having the
+# Makefiles attemp to copy non-existant DLL PDBs.
+# TODO: Fix up zeroc-ice-3.5.0-win32-static.diff to not
+# attemp to install .pdbs for static libraries.
+cmd /c nmake /f Makefile.mak PREFIX="$(cygpath -w ${MUMBLE_PREFIX}/ZeroC-Ice)" STATICLIBS=yes OPTIMIZE=yes RELEASEPDBS=no HAS_MFC=no ARFLAGS="${ICE_ARFLAGS}" LDFLAGS="${ICE_LDFLAGS}" CPPFLAGS="${ICE_CPPFLAGS}" install
