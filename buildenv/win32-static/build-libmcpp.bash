c1701d1ec33cf03eefa7e9a908a1617b0ddec9ec
Mikkel Krautz <mikkel@krautz.dk>
2013-08-18 15:57:24 +0200

    buildenv/win32-static: generate .pdb for libmcpp.

--- buildenv/win32-static/build-libmcpp.bash
+++ buildenv/win32-static/build-libmcpp.bash
@@ -6,7 +6,8 @@ expect_sha1 "mcpp-2.7.2.tar.gz" "703356b7c2cd30d7fb6000625bf3ccc2eb977ecb"
 
 tar -zxf mcpp-2.7.2.tar.gz
 cd mcpp-2.7.2
-patch -p1  < ${MUMBLE_BUILDENV_ROOT}/patches/zeroc-patch.mcpp.2.7.2
+patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/zeroc-patch.mcpp.2.7.2
+patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/mcpp-pdb.patch
 cd src
 patch --binary -p0 < ../noconfig/vc2010.dif
 cmd /c nmake MCPP_LIB=1 /f ..\\noconfig\\visualc.mak mcpplib_lib
