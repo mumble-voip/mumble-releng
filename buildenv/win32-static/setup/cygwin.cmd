8866fbd644f3beda3b36c9ad196257ae9f3e5811
Mikkel Krautz <mikkel@krautz.dk>
2013-09-06 14:29:03 +0200

    buildenv/win32-static: source /etc/profile in cygwin launcher shortcut; remove PATH kludge from env script.

--- buildenv/win32-static/setup/cygwin.cmd
+++ buildenv/win32-static/setup/cygwin.cmd
@@ -4,4 +4,5 @@
 
 @echo off
 
-c:\cygwin\bin\bash.exe -c "source ./env && bash"
+for /f %%I in ('c:\cygwin\bin\cygpath %MUMBLE_PREFIX%') do set BOOTSTRAP_CYGWIN_MUMBLE_PREFIX=%%I
+c:\cygwin\bin\bash.exe -c "source /etc/profile && source ${BOOTSTRAP_CYGWIN_MUMBLE_PREFIX}/env && cd ${MUMBLE_PREFIX} && bash"
