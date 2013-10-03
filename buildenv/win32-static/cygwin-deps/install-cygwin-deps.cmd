068788e0d414c343a6d9326f8ac38acea6fc422b
Zuko <zuczeq@gmail.com>
2013-10-03 18:24:54 +0200

    buildenv/win32-static: install-cygwin-deps.cmd fixes

--- buildenv/win32-static/cygwin-deps/install-cygwin-deps.cmd
+++ buildenv/win32-static/cygwin-deps/install-cygwin-deps.cmd
@@ -7,13 +7,18 @@
 
 @echo off
 
-dir setup-x86.exe >NUL 2>NUL
-if "%errorlevel%"=="0" (
-	goto install
+TITLE Install Cygwin Dependencies
+
+SET filename="" 
+IF EXIST setup-x86.exe (
+	SET filename="setup-x86.exe" 
+    goto install
 ) else (
-	dir setup-x86_64.exe >NUL 2>NUL
-	if "%errorlevel%"=="0" (
+	IF EXIST setup-x86_64.exe (
+		SET filename="setup-x86_64.exe" 
 		goto install
+	) else (
+		goto error
 	)
 )
 
@@ -31,10 +36,15 @@ exit /b
 
 :: This list of packages should be kept in sync with
 :: the ones document in the build env's README file.
-setup-x86.exe -q -P bzip2,ca-certificates,curl,diffstat,diffutils,^
+%filename% -q -P bzip2,ca-certificates,curl,diffstat,diffutils,^
 dos2unix,file,findutils,git,grep,gzip,less,make,man,mingw-binutils,^
 mingw-gcc-core,mingw-gcc-g++,mingw-pthreads,mingw-runtime,mingw-w32api,^
 openssh,patch,patchutils,perl,perl-Error,perl_vendor,pkg-config,sed,tar,^
 unzip,util-linux,vim,which,xz,zlib0
 
+@echo off
+
+echo.
+echo Done.
+echo.
 pause
