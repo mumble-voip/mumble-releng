dbc10f455066ed9e98a183767e71292205c2c8df
Mikkel Krautz <mikkel@krautz.dk>
2013-10-23 17:03:14 +0200

    buildenv/win32-static: update Qt 5 to 5.2-beta1.

--- buildenv/win32-static/build-qt5.bash
+++ buildenv/win32-static/build-qt5.bash
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
 source common.bash
-fetch_if_not_exists "http://download.qt-project.org/official_releases/qt/5.1/5.1.1/single/qt-everywhere-opensource-src-5.1.1.zip"
-expect_sha1 "qt-everywhere-opensource-src-5.1.1.zip" "54271a90951eb356f138ac71a912d3937c62d9e9"
+fetch_if_not_exists "http://download.qt-project.org/development_releases/qt/5.2/5.2.0-beta1/single/qt-everywhere-opensource-src-5.2.0-beta1.zip"
+expect_sha1 "qt-everywhere-opensource-src-5.2.0-beta1.zip" "2059a73d7d4bcc6f4744d9731687d39b894f3b6e"
 
-unzip -q -o qt-everywhere-opensource-src-5.1.1.zip
-cd qt-everywhere-opensource-src-5.1.1
+unzip -q -o qt-everywhere-opensource-src-5.2.0-beta1.zip
+cd qt-everywhere-opensource-src-5.2.0-beta1
 
 chmod +x qtbase/configure.exe
 chmod +x gnuwin32/bin/*
@@ -14,6 +14,6 @@ patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/qt5-mariadb-support.patch
 patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/qt5-qsvg-system-zlib-support.patch
 patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/qt5-qtimageformats-system-zlib-support.patch
 
-cmd /c configure -release -static -no-c++11 -skip qtwebkit -skip qtwebkit-examples -prefix $(cygpath -w ${MUMBLE_PREFIX}/Qt5.1) -qt-sql-sqlite -qt-sql-mysql -I $(cygpath -w ${MUMBLE_PREFIX}/mariadbclient/mariadbclient/include) -L $(cygpath -w ${MUMBLE_PREFIX}/mariadbclient/lib) -system-zlib -I $(cygpath -w ${MUMBLE_PREFIX}/zlib/include) -L $(cygpath -w ${MUMBLE_PREFIX}/zlib/lib) ZLIB_LIBS="-lzlib" -qt-libpng -qt-libjpeg -openssl-linked -I $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/include) -L $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/lib) OPENSSL_LIBS="-llibeay32 -lssleay32 -lcrypt32 -lgdi32" -platform win32-msvc2010 -no-dbus -nomake examples -nomake tests -ltcg -mp -opensource -confirm-license
+cmd /c configure -release -static -no-c++11 -skip qtwebkit -skip qtwebkit-examples -prefix $(cygpath -w ${MUMBLE_PREFIX}/Qt5.2) -qt-sql-sqlite -qt-sql-mysql -I $(cygpath -w ${MUMBLE_PREFIX}/mariadbclient/mariadbclient/include) -L $(cygpath -w ${MUMBLE_PREFIX}/mariadbclient/lib) -system-zlib -I $(cygpath -w ${MUMBLE_PREFIX}/zlib/include) -L $(cygpath -w ${MUMBLE_PREFIX}/zlib/lib) ZLIB_LIBS="-lzlib" -qt-libpng -qt-libjpeg -openssl-linked -I $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/include) -L $(cygpath -w ${MUMBLE_PREFIX}/OpenSSL/lib) OPENSSL_LIBS="-llibeay32 -lssleay32 -lcrypt32 -lgdi32" -platform win32-msvc2010 -no-dbus -nomake examples -nomake tests -ltcg -mp -opensource -confirm-license
 cmd /c nmake
 cmd /c nmake install
