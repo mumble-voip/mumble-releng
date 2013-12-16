a1e857622054ceb3e203e4cf87e9ffc26773726b
Mikkel Krautz <mikkel@krautz.dk>
2013-12-16 23:06:58 +0100

    buildenv/osx-universal: add -search_paths_first to Qt 4's OpenSSL linker flags.
    The -search_paths_first flag forces ld to search each lib directory
    for *both* a .dylib and an .a, instead of first searching each directory
    for a .dylib, and failing that, searching them for an .a-version instead.
    
    Without this change, our static OpenSSL libs are not considered, and Qt
    (and deps) will link against the dynamic system-shipped OpenSSL libraries
    instead.

--- buildenv/osx-universal/build-qt4.bash
+++ buildenv/osx-universal/build-qt4.bash
@@ -12,6 +12,6 @@ unset CXXFLAGS
 unset LDFLAGS
 export CFLAGS="-I$MUMBLE_PREFIX/include"
 export CXXFLAGS="-I$MUMBLE_PREFIX/include"
-OPENSSL_LIBS="-L$MUMBLE_PREFIX/lib -lssl -lcrypto" ./configure -static -no-reduce-exports -no-pch -fast -nomake examples -nomake demos -nomake docs -debug-and-release -universal -carbon -qt-sql-sqlite -no-dbus -no-webkit -no-phonon -no-phonon-backend -no-qt3support -no-multimedia -no-audio-backend -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -openssl-linked -mysql_config no -sdk ${OSX_SDK} -prefix ${MUMBLE_PREFIX}/Qt4.8 -opensource -confirm-license
+OPENSSL_LIBS="-Wl,-search_paths_first -L$MUMBLE_PREFIX/lib -lssl -lcrypto" ./configure -static -no-reduce-exports -no-pch -fast -nomake examples -nomake demos -nomake docs -debug-and-release -universal -carbon -qt-sql-sqlite -no-dbus -no-webkit -no-phonon -no-phonon-backend -no-qt3support -no-multimedia -no-audio-backend -qt-zlib -qt-libtiff -qt-libpng -qt-libmng -qt-libjpeg -openssl-linked -mysql_config no -sdk ${OSX_SDK} -prefix ${MUMBLE_PREFIX}/Qt4.8 -opensource -confirm-license
 make
 make install
