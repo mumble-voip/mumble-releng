f85ec329142af3adde26c9aa28543978a224190b
Mikkel Krautz <mikkel@krautz.dk>
2013-12-16 23:38:12 +0100

    buildenv/centos-ermine: update to zlib 1.2.8.

--- buildenv/centos-ermine/build-zlib.bash
+++ buildenv/centos-ermine/build-zlib.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-SHA1="4aa358a95d1e5774603e6fa149c926a80df43559"
-curl -O "http://zlib.net/zlib-1.2.7.tar.gz"
-if [ "$(sha1sum zlib-1.2.7.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="a4d316c404ff54ca545ea71a27af7dbc29817088"
+curl -O "http://zlib.net/zlib-1.2.8.tar.gz"
+if [ "$(sha1sum zlib-1.2.8.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo zlib checksum mismatch
 	exit
 fi
-tar -zxf zlib-1.2.7.tar.gz
-cd zlib-1.2.7
+tar -zxf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
 ./configure --prefix=${MUMBLE_PREFIX}
 make
 make install
