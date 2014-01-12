8632440550668dadf82c1186c696548f72c2db50
Mikkel Krautz <mikkel@krautz.dk>
2014-01-12 17:06:34 +0100

    buildenv/win32-static: update nasm to 2.11.

--- buildenv/win32-static/install-nasm-binary.bash
+++ buildenv/win32-static/install-nasm-binary.bash
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 
 source common.bash
-fetch_if_not_exists "http://www.nasm.us/pub/nasm/releasebuilds/2.10.09/win32/nasm-2.10.09-win32.zip"
-expect_sha1 "nasm-2.10.09-win32.zip" "4c4e70517deb2fac4aa1e6390ce7535d7a50206b"
+fetch_if_not_exists "http://www.nasm.us/pub/nasm/releasebuilds/2.11/win32/nasm-2.11-win32.zip"
+expect_sha1 "nasm-2.11-win32.zip" "d02cd111fc74daaf901a714d4d325fef5f769224"
+expect_sha256 "nasm-2.11-win32.zip" "abcad8089bdf03c10e0cd621415ae71cd8289d346000ee8b1e8aaf27f6d98de9"
 
-unzip -o nasm-2.10.09-win32.zip
+unzip -o nasm-2.11-win32.zip
 mkdir -p ${MUMBLE_PREFIX}/nasm/
-cp -R nasm-2.10.09/* ${MUMBLE_PREFIX}/nasm/
+cp -R nasm-2.11/* ${MUMBLE_PREFIX}/nasm/
 
 # FLAC's MSVC build wants nasmw.exe.
 cp ${MUMBLE_PREFIX}/nasm/nasm.exe ${MUMBLE_PREFIX}/nasm/nasmw.exe
