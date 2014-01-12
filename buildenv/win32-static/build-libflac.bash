c29925cb3902e82c3149c4d082718fa7f3f60d4d
Mikkel Krautz <mikkel@krautz.dk>
2014-01-12 16:54:06 +0100

    buildenv/win32-static: update FLAC to 1.3.0.

--- buildenv/win32-static/build-libflac.bash
+++ buildenv/win32-static/build-libflac.bash
@@ -1,13 +1,12 @@
 #!/bin/bash -ex
 
 source common.bash
-fetch_if_not_exists "http://downloads.xiph.org/releases/flac/flac-1.2.1.tar.gz"
-expect_sha1 "flac-1.2.1.tar.gz" "bd54354900181b59db3089347cc84ad81e410b38"
+fetch_if_not_exists "http://downloads.xiph.org/releases/flac/flac-1.3.0.tar.xz"
+expect_sha1 "flac-1.3.0.tar.xz" "a136e5748f8fb1e6c524c75000a765fc63bb7b1b"
+expect_sha256 "flac-1.3.0.tar.xz" "fa2d64aac1f77e31dfbb270aeb08f5b32e27036a52ad15e69a77e309528010dc"
 
-tar -zxf flac-1.2.1.tar.gz
-cd flac-1.2.1
-
-patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/libflac-ftello-fseeko-msvc2013.patch
+tar -Jxf flac-1.3.0.tar.xz
+cd flac-1.3.0
 
 cp -R ${MUMBLE_SNDFILE_PREFIX}/include/ogg include/ogg
 
