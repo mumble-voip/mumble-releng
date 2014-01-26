5cad214f2994e1e5db5485400d76fd1283a72e42
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 21:44:07 +0100

    buildenv/centos-ermine: update Boost to 1.55.0.

--- buildenv/centos-ermine/build-boost.bash
+++ buildenv/centos-ermine/build-boost.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-SHA1="e6dd1b62ceed0a51add3dda6f3fc3ce0f636a7f3"
-curl -L -O "http://downloads.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.tar.bz2"
-if [ "$(sha1sum boost_1_53_0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
+SHA1="cef9a0cc7084b1d639e06cd3bc34e4251524c840"
+curl -L -O "http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2"
+if [ "$(sha1sum boost_1_55_0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
 	echo boost checksum mismatch
 	exit
 fi
-tar -jxf boost_1_53_0.tar.bz2
-cd boost_1_53_0
-./bootstrap.sh --without-libraries=atomic,chrono,context,date_time,exception,filesystem,graph,graph_parallel,iostreams,locale,math,mpi,program_options,python,random,regex,serialization,signals,system,test,thread,timer,wave --prefix=${MUMBLE_PREFIX}
+tar -jxf boost_1_55_0.tar.bz2
+cd boost_1_55_0
+./bootstrap.sh --without-libraries=atomic,chrono,context,coroutine,date_time,exception,filesystem,graph,graph_parallel,iostreams,locale,log,math,mpi,program_options,python,random,regex,serialization,signals,system,test,thread,timer,wave --prefix=${MUMBLE_PREFIX}
 ./b2 install
