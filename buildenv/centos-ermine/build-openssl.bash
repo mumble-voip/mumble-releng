685b442b057c189ffb8c49f607b74326dd432df5
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 20:52:20 +0100

    buildenv/centos-ermine: update openssl to 1.0.0l.

--- buildenv/centos-ermine/build-openssl.bash
+++ buildenv/centos-ermine/build-openssl.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="ec5d20f1ee52ae765b9286e9d7951dcfc9548607"
-curl -O http://www.openssl.org/source/openssl-1.0.0k.tar.gz
-if [ "$(sha1sum openssl-1.0.0k.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="f7aeaa76a043ab9c1cd5899d09c696d98278e2d7"
+curl -O "http://www.openssl.org/source/openssl-1.0.0l.tar.gz"
+if [ "$(sha1sum openssl-1.0.0l.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo openssl checksum mismatch
 	exit
 fi
-tar -zxf openssl-1.0.0k.tar.gz
-cd openssl-1.0.0k
+tar -zxf openssl-1.0.0l.tar.gz
+cd openssl-1.0.0l
 ./Configure linux-elf shared zlib threads --prefix=$MUMBLE_PREFIX --openssldir=$MUMBLE_PREFIX/openssl -L$MUMBLE_PREFIX/lib -I$MUMBLE_PREFIX/include
 make
 make install
