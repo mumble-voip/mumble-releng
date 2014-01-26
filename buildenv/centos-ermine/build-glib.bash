7d7e1f2ee9e62c448f40f4ffe82aab2ef52acb28
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 21:12:44 +0100

    buildenv/centos-ermine: update glib to 2.38.2.

--- buildenv/centos-ermine/build-glib.bash
+++ buildenv/centos-ermine/build-glib.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
-SHA1="f2b94ca757191dddba686e54b32b3dfc5ad5d8fb"
-curl -O "http://ftp.gnome.org/pub/GNOME/sources/glib/2.34/glib-2.34.3.tar.xz"
-if [ "$(sha1sum glib-2.34.3.tar.xz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="685c5a4215b776b83dd5330ab9084c5dcb0a51b8"
+curl -O "ftp://ftp.gnome.org/pub/gnome/sources/glib/2.38/glib-2.38.2.tar.xz"
+if [ "$(sha1sum glib-2.38.2.tar.xz | cut -b -40)" != "${SHA1}" ]; then
 	echo glib checksum mismatch
 	exit
 fi
-xzcat glib-2.34.3.tar.xz | tar -xf -
-cd glib-2.34.3
+xzcat glib-2.38.2.tar.xz | tar -xf -
+cd glib-2.38.2
+export CFLAGS="-march=i486"
 ./configure --prefix=$MUMBLE_PREFIX
 make
 make install
