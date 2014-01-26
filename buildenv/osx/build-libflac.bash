f8ed89f5ce77d8a7b930af1c071a567deab3c211
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 23:53:05 +0100

    buildenv/osx: update FLAC to 1.2.1.

--- buildenv/osx/build-libflac.bash
+++ buildenv/osx/build-libflac.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="bd54354900181b59db3089347cc84ad81e410b38"
-curl -O "http://downloads.xiph.org/releases/flac/flac-1.2.1.tar.gz"
-if [ "$(shasum -a 1 flac-1.2.1.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="a136e5748f8fb1e6c524c75000a765fc63bb7b1b"
+curl -O "http://downloads.xiph.org/releases/flac/flac-1.3.0.tar.xz"
+if [ "$(shasum -a 1 flac-1.3.0.tar.xz | cut -b -40)" != "${SHA1}" ]; then
 	echo flac checksum mismatch
 	exit
 fi
-tar -zxf flac-1.2.1.tar.gz
-cd flac-1.2.1
+xzcat flac-1.3.0.tar.xz | tar -xf -
+cd flac-1.3.0
 ./configure --build=x86_64-apple-darwin$(uname -r) --prefix=$MUMBLE_PREFIX --disable-shared --enable-static
 make
 make install
