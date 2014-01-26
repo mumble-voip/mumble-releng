9d249bc7e806ed6f796817a21a27040411fc491a
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 21:41:18 +0100

    buildenv/osx: update OpenSSL to 1.0.0l.

--- buildenv/osx/build-openssl.bash
+++ buildenv/osx/build-openssl.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="ec5d20f1ee52ae765b9286e9d7951dcfc9548607"
-curl -O http://www.openssl.org/source/openssl-1.0.0k.tar.gz
-if [ "$(shasum -a 1 openssl-1.0.0k.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="f7aeaa76a043ab9c1cd5899d09c696d98278e2d7"
+curl -O "http://www.openssl.org/source/openssl-1.0.0l.tar.gz"
+if [ "$(shasum -a 1 openssl-1.0.0l.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo openssl checksum mismatch
 	exit
 fi
-tar -zxf openssl-1.0.0k.tar.gz
-cd openssl-1.0.0k
+tar -zxf openssl-1.0.0l.tar.gz
+cd openssl-1.0.0l
 ./Configure darwin64-x86_64-cc no-shared --prefix=$MUMBLE_PREFIX --openssldir=$MUMBLE_PREFIX/openssl
 make
 make install
