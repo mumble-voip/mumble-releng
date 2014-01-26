1a4bc715c446ceba7c352f40409eafe84cd64d30
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 23:50:22 +0100

    buildenv/osx: update protobuf to 2.5.0.

--- buildenv/osx/build-protobuf.bash
+++ buildenv/osx/build-protobuf.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="df5867e37a4b51fb69f53a8baf5b994938691d6d"
-curl -O "http://protobuf.googlecode.com/files/protobuf-2.4.1.tar.bz2"
-if [ "$(shasum -a 1 protobuf-2.4.1.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
+SHA1="62c10dcdac4b69cc8c6bb19f73db40c264cb2726"
+curl -O "http://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2"
+if [ "$(shasum -a 1 protobuf-2.5.0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
 	echo protobuf checksum mismatch
 	exit
 fi
-tar -jxf protobuf-2.4.1.tar.bz2
-cd protobuf-2.4.1
+tar -jxf protobuf-2.5.0.tar.bz2
+cd protobuf-2.5.0
 ./configure --disable-dependency-tracking --prefix=$MUMBLE_PREFIX --disable-shared --enable-static
 make
 make install
