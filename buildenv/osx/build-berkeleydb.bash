28708a5954ca53136659541197bdbcaa9a27695c
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 23:49:21 +0100

    buildenv/osx: update BerkeleyDB to 5.3.28.

--- buildenv/osx/build-berkeleydb.bash
+++ buildenv/osx/build-berkeleydb.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="32e43c4898c8996750c958a90c174bd116fcba83"
-curl -O "http://download.oracle.com/berkeley-db/db-5.3.21.tar.gz"
-if [ "$(shasum -a 1 db-5.3.21.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="fa3f8a41ad5101f43d08bc0efb6241c9b6fc1ae9"
+curl -O "http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz"
+if [ "$(shasum -a 1 db-5.3.28.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo berkeleydb checksum mismatch
 	exit
 fi
-tar -zxf db-5.3.21.tar.gz
-cd db-5.3.21
+tar -zxf db-5.3.28.tar.gz
+cd db-5.3.28
 patch -p1 < ../patches/db-5.3.21-clang.patch
 cd build_unix
 ../dist/configure --prefix=$MUMBLE_PREFIX --disable-shared --enable-static --enable-cxx
