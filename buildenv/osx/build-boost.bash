d770f72ea3613ab39546c9624e751b5449a23d7c
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 23:49:56 +0100

    buildenv/osx: update Boost to 1.55.0.

--- buildenv/osx/build-boost.bash
+++ buildenv/osx/build-boost.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
-SHA1="230782c7219882d0fab5f1effbe86edb85238bf4"
-curl -L -O "http://downloads.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2"
-if [ "$(shasum -a 1 boost_1_54_0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
+SHA1="cef9a0cc7084b1d639e06cd3bc34e4251524c840"
+curl -L -O "http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2"
+if [ "$(shasum -a 1 boost_1_55_0.tar.bz2 | cut -b -40)" != "${SHA1}" ]; then
 	echo boost checksum mismatch
 	exit
 fi
-tar -jxf boost_1_54_0.tar.bz2
-rm -rf $MUMBLE_PREFIX/include/boost_1_54_0
-cd boost_1_54_0
-patch -p1 < ../patches/boost-1.48.0-nil-fix.patch
-cd ..
-mv boost_1_54_0 $MUMBLE_PREFIX/include/
+tar -jxf boost_1_55_0.tar.bz2
+rm -rf $MUMBLE_PREFIX/include/boost_1_55_0
+cd boost_1_55_0
+./bootstrap.sh --prefix=${MUMBLE_PREFIX} --without-libraries=atomic,chrono,coroutine,context,date_time,exception,filesystem,graph,graph_parallel,iostreams,locale,log,math,mpi,program_options,python,random,regex,serialization,signals,system,test,thread,timer,wave
+./b2 install
