3d265bbe8414b718861c245c0bded4b8afadae87
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 23:53:31 +0100

    buildenv/osx: update libogg to 1.3.1.

--- buildenv/osx/build-libogg.bash
+++ buildenv/osx/build-libogg.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-SHA1="a900af21b6d7db1c7aa74eb0c39589ed9db991b8"
-curl -O "http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
-if [ "$(shasum -a 1 libogg-1.3.0.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="270685c2a3d9dc6c98372627af99868aa4b4db53"
+curl -O "http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz"
+if [ "$(shasum -a 1 libogg-1.3.1.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo libogg checksum mismatch
 	exit
 fi
-tar -zxf libogg-1.3.0.tar.gz
-cd libogg-1.3.0
+tar -zxf libogg-1.3.1.tar.gz
+cd libogg-1.3.1
 ./configure --disable-dependency-tracking --prefix=$MUMBLE_PREFIX --disable-shared --enable-static
 make
 make install
