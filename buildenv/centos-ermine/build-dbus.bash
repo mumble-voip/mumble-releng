4bef82458aae85517eaf9c6cc9c7229da3068d5e
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 21:16:00 +0100

    buildenv/centos-ermine: update dbus to 1.8.0.

--- buildenv/centos-ermine/build-dbus.bash
+++ buildenv/centos-ermine/build-dbus.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-SHA1="d9634807d1de9b64727ae2178e3af2227fca0fca"
-curl -O "http://dbus.freedesktop.org/releases/dbus/dbus-1.6.8.tar.gz"
-if [ "$(sha1sum dbus-1.6.8.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="d14ab33e92e29fa732cdff69214913832181e737"
+curl -O "http://dbus.freedesktop.org/releases/dbus/dbus-1.8.0.tar.gz"
+if [ "$(sha1sum dbus-1.8.0.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo dbus checksum mismatch
 	exit
 fi
-tar -zxf dbus-1.6.8.tar.gz
-cd dbus-1.6.8
+tar -zxf dbus-1.8.0.tar.gz
+cd dbus-1.8.0
 CFLAGS="-L${MUMBLE_PREFIX}/lib -I${MUMBLE_PREFIX}/include" ./configure --prefix=$MUMBLE_PREFIX --with-xml=expat --with-system-socket=/var/run/dbus/system_bus_socket
 make
 make install
