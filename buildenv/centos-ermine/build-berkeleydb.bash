3aec7244fa2e13838648ce1172f7e737f4c542a1
Mikkel Krautz <mikkel@krautz.dk>
2014-01-26 22:16:22 +0100

    buildenv/centos-ermine: update BerkeleyDB to 5.3.28.

--- buildenv/centos-ermine/build-berkeleydb.bash
+++ buildenv/centos-ermine/build-berkeleydb.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-SHA1="32e43c4898c8996750c958a90c174bd116fcba83"
-curl -O "http://download.oracle.com/berkeley-db/db-5.3.21.tar.gz"
-if [ "$(sha1sum db-5.3.21.tar.gz | cut -b -40)" != "${SHA1}" ]; then
+SHA1="fa3f8a41ad5101f43d08bc0efb6241c9b6fc1ae9"
+curl -O "http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz"
+if [ "$(sha1sum db-5.3.28.tar.gz | cut -b -40)" != "${SHA1}" ]; then
 	echo berkeleydb checksum mismatch
 	exit
 fi
-tar -zxf db-5.3.21.tar.gz
-cd db-5.3.21/build_unix
+tar -zxf db-5.3.28.tar.gz
+cd db-5.3.28/build_unix
 ../dist/configure --prefix=$MUMBLE_PREFIX --enable-cxx
 make
 make install
