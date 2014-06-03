4d60057dae204bb69d48e9ca59ce85d05f74f17b
Mikkel Krautz <mikkel@krautz.dk>
2014-06-03 21:20:12 +0200

    buildscripts/1.2.x: add tarball.bash for generating our 1.2.x source tarballs.

---
+++ buildscripts/1.2.x/tarball.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+# Copyright 2014 The 'mumble-releng' Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that
+# can be found in the LICENSE file in the source tree or at
+# <http://mumble.info/mumble-releng/LICENSE>.
+
+# 1.2.x needs chmod +x
+chmod +x ./scripts/release.pl
+./scripts/release.pl
