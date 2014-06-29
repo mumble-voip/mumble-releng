e23f67fc5d657bb75302d81802a414ac44832740
Mikkel Krautz <mikkel@krautz.dk>
2014-06-29 15:36:14 +0200

    buildenv/1.2.x/win32: actually use new bzip2 dll+pdb patch.

--- buildenv/1.2.x/win32/build-bzip2.bash
+++ buildenv/1.2.x/win32/build-bzip2.bash
@@ -11,10 +11,11 @@ expect_sha1 "bzip2-1.0.6.tar.gz" "3f89f861209ce81a6bab1fd1998c0ef311712002"
 tar -zxf bzip2-1.0.6.tar.gz
 cd bzip2-1.0.6
 patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/bzip2-linker-pdb.patch
-patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/bzip2-dll.patch
+patch -p1 < ${MUMBLE_BUILDENV_ROOT}/patches/bzip2-dll+pdb.patch
 cmd /c nmake /f makefile.msc bzip2.dll
 
 mkdir -p ${MUMBLE_PREFIX}/bzip2/{include,lib}
 cp libbz2.lib ${MUMBLE_PREFIX}/bzip2/lib/
 cp bzip2.dll ${MUMBLE_PREFIX}/bzip2/lib/
+cp bzip2.pdb ${MUMBLE_PREFIX}/bzip2/lib/
 cp bzlib.h ${MUMBLE_PREFIX}/bzip2/include/
