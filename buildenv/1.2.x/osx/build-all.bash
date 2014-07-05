f0f25b28603bc3cf691c1ac83492971c2309b03c
Mikkel Krautz <mikkel@krautz.dk>
2014-07-05 19:19:40 +0200

    buildenv/1.2.x/osx, buildenv/1.2.x/osx-universal, buildenv/1.3.x/osx: fix 'build-all.bash' to set up the PATH env var to include 'mumble-build'.

--- buildenv/1.2.x/osx/build-all.bash
+++ buildenv/1.2.x/osx/build-all.bash
@@ -4,6 +4,9 @@
 # can be found in the LICENSE file in the source tree or at
 # <http://mumble.info/mumble-releng/LICENSE>.
 
+export MUMBLE_RELENG_ROOT=$(git rev-parse --show-toplevel)
+export PATH=${MUMBLE_RELENG_ROOT}/mumble-build:${PATH}
+
 # Utils
 ./pkgconfig.build
 ./libtool.build
