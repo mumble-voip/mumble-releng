2db1f783994b61b53a8423f106b856227a01e4e3
Mikkel Krautz <mikkel@krautz.dk>
2014-07-27 15:27:02 +0200

    buildscripts/1.3.x: update mumble-win32-static.cmd and mumble-win64-static.cmd for Qt 5.

--- buildscripts/1.3.x/mumble-win32-static.cmd
+++ buildscripts/1.3.x/mumble-win32-static.cmd
@@ -14,9 +14,9 @@ if errorlevel 1 exit /b errorlevel
 
 echo Build mumble
 if "%MUMBLE_BUILD_TYPE%" == "Release" (
-	qmake CONFIG-=sse2 CONFIG+="release static symbols packaged" DEFINES+="MUMBLE_VERSION=%mumblebuildversion%" -recursive
+	qmake CONFIG+="release static symbols packaged" DEFINES+="MUMBLE_VERSION=%mumblebuildversion%" -recursive
 ) else (
-	qmake CONFIG-=sse2 CONFIG+="release static symbols packaged" DEFINES+="MUMBLE_VERSION=%mumblebuildversion% SNAPSHOT_BUILD=1" -recursive
+	qmake CONFIG+="release static symbols packaged" DEFINES+="MUMBLE_VERSION=%mumblebuildversion% SNAPSHOT_BUILD=1" -recursive
 )
 if errorlevel 1 exit /b errorlevel
 nmake release
@@ -24,7 +24,9 @@ if errorlevel 1 exit /b errorlevel
 
 echo Build SSE2 versions of opus
 cd opus-build
-qmake -recursive
+nmake clean
+if errorlevel 1 exit /b errorlevel
+qmake -recursive CONFIG+=sse2
 if errorlevel 1 exit /b errorlevel
 nmake release
 if errorlevel 1 exit /b errorlevel
@@ -32,7 +34,9 @@ cd ..
 
 echo Build SSE2 versions of celt 0.11.0
 cd celt-0.11.0-build
-qmake -recursive
+nmake clean
+if errorlevel 1 exit /b errorlevel
+qmake -recursive CONFIG+=sse2
 if errorlevel 1 exit /b errorlevel
 nmake release
 if errorlevel 1 exit /b errorlevel
@@ -40,7 +44,9 @@ cd ..
 
 echo Build SSE2 versions of celt 0.7.0
 cd celt-0.7.0-build
-qmake -recursive
+nmake clean
+if errorlevel 1 exit /b errorlevel
+qmake -recursive CONFIG+=sse2
 if errorlevel 1 exit /b errorlevel
 nmake release
 if errorlevel 1 exit /b errorlevel
