36be22095cc5acf7d9e8a25ba207caa80f4f9612
Mikkel Krautz <mikkel@krautz.dk>
2014-11-01 09:29:02 +0100

    tools: add arch flag to collect_symbols.py.

--- tools/collect_symbols.py
+++ tools/collect_symbols.py
@@ -53,7 +53,8 @@ def collect(args):
 
     buildinfo = {"type" : args.buildtype,
                  "version" : args.version,
-                 "product" : args.product }
+                 "product" : args.product,
+                 "arch" : args.arch }
 
     debug("Writing buildinfo to '%s', content: %s", buildfile, repr(buildinfo))
     
@@ -188,7 +189,8 @@ if __name__ == "__main__":
     RELEASE = 'Release'
 
     BUILD_TYPES = [CI, SNAPSHOT, BETA, RC, RELEASE]
-    
+    BUILD_ARCHS = ['x86', 'x64']
+
     parent_parser = ArgumentParser(description = 'Collects and updates symbol archives to feed into the symbolstore')
     subparsers = parent_parser.add_subparsers(help = 'action', dest='action')
     
@@ -198,6 +200,7 @@ if __name__ == "__main__":
     collect_parser.add_argument('--version', help = 'Build version', required = True)
     collect_parser.add_argument('--buildtype', help = 'Build type', choices = BUILD_TYPES, required = True)
     collect_parser.add_argument('--product', help = 'Build product', required = True)
+    collect_parser.add_argument('--arch', help = 'Build architecture', choices = BUILD_ARCHS, required = True)
     
     collect_parser = subparsers.add_parser('update', help = 'Replace binary files in archive with ones from the msi')
     collect_parser.add_argument('archive', help = 'Source archive')
