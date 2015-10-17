765bc7f0ae2072bc33df8b9450f597ef35b2c133
Mikkel Krautz <mikkel@krautz.dk>
2015-10-17 20:22:09 +0200

    buildenv/1.3.x: update buildenv naming scheme to include commit count.

--- buildenv/1.3.x/centos-ermine/setup/name.bash
+++ buildenv/1.3.x/centos-ermine/setup/name.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Copyright 2014 The 'mumble-releng' Authors. All rights reserved.
+# Copyright 2014-2015 The 'mumble-releng' Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that
 # can be found in the LICENSE file in the source tree or at
 # <http://mumble.info/mumble-releng/LICENSE>.
 
-NAME=$(git log -n 1 --date=short --pretty="format:centos-ermine-1.3.x-%ad-%h")
+COUNT=$(git rev-list HEAD --count)
+NAME=$(git log -n 1 --date=short --pretty="format:centos-ermine-1.3.x-%ad-%h-${COUNT}")
 if [ ! -z "$(git status --porcelain)" ]; then
 	NAME="${NAME}~dirty"
 fi
--- buildenv/1.3.x/win32-static/setup/name-debug.cmd
+++ buildenv/1.3.x/win32-static/setup/name-debug.cmd
@@ -1,6 +1,8 @@
-:: Copyright 2013-2014 The 'mumble-releng' Authors. All rights reserved.
+:: Copyright 2013-2015 The 'mumble-releng' Authors. All rights reserved.
 :: Use of this source code is governed by a BSD-style license that
 :: can be found in the LICENSE file in the source tree or at
 :: <http://mumble.info/mumble-releng/LICENSE>.
 
-git log -n 1 --date=short --pretty="format:win32-static-debug-1.3.x-%%ad-%%h"
+@echo off
+for /f "delims=" %%i in ('git rev-list HEAD --count') do set count=%%i
+git log -n 1 --date=short --pretty="format:win32-static-debug-1.3.x-%%ad-%%h-%count%"
