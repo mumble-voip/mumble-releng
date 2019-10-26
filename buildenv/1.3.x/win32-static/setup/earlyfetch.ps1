0e0fd7c351aaccb27082cd91f45ce63181b4006b
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2019-10-26 19:06:18 +0300

    Add check for PowerShell < 4.0

--- buildenv/1.3.x/win32-static/setup/earlyfetch.ps1
+++ buildenv/1.3.x/win32-static/setup/earlyfetch.ps1
@@ -1,8 +1,18 @@
 #!/usr/bin/env mumble-build
-# Copyright 2013-2017 The 'mumble-releng' Authors. All rights reserved.
+# Copyright 2013-2019 The 'mumble-releng' Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that
 # can be found in the LICENSE file in the source tree or at
 # <http://mumble.info/mumble-releng/LICENSE>.
+
+if ($PSVersionTable.PSVersion.Major -lt 4) {
+	Write-Warning 'This script uses cmdlet Get-FileHash.
+Please, upgrade your Windows Management Framework: https://microsoft.com/download/40855'
+	if ((Read-Host -Prompt "Open download link now? y/n") -eq 'y') {
+		Start-Process https://microsoft.com/download/40855
+	}
+	exit 1
+}
+
 
 $target = $Args[0]
 $url = $Args[1]
